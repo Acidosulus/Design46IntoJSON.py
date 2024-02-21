4f62bf8e84b2f8add6d82e26fe0cec8479a04b4c
Николай Михайлович Казаковцев <Acidos@yandex.ru>
2024-02-21 14:21:35 +0700

    Wed 02/21/2024 14:21:35.62

--- Design46IntoJSON.py
+++ Design46IntoJSON.py
@@ -1,155 +1,80 @@
 import sys
 from openpyxl import load_workbook
+from click import echo, style
 
-print(sys.argv[1])
+import pprint
+printer = pprint.PrettyPrinter(indent=12, width=180)
+prnt = printer.pprint
 
-file_source = sys.argv[1]
-if file_source[-3:].upper() == 'ODS':
+echo(style(text=sys.argv[1], bg='red', fg='bright_yellow'))
 
-	print('!!!!!!!!!!!!!!!!!!!!!!!!!')
+def join_tuple_into_string(source:tuple):
+	result = ''
+	for elem in source:
+		if type(elem)==str:
+			result += elem
+		if type(elem)==tuple:
+			result += join_tuple_into_string(elem)
+	return result
+
+def read_excel_range(file_name, sheet_name, start_row, end_row, start_col, end_col):
 	import win32com.client as win32
+	excel = win32.Dispatch('Excel.Application')
+	workbook = excel.Workbooks.Open(file_name)
+	sheet = workbook.Sheets[sheet_name]
+	range_cells = sheet.Range(sheet.Cells(start_row, start_col), sheet.Cells(end_row, end_col))
+	data = []
+	for row in range_cells.Rows:
+		row_data = []
+		for cell in row:
+			if not cell.Hidden:
+				if cell.Value2!=None:
+					if type(cell.Value2)==tuple:
+						if  len(cell.Value2)>0:
+							val = cell.Value2
+							val = join_tuple_into_string(val)
+							row_data.append(val)
+					else:
+						row_data.append(str(cell.Value2))
+		if len(row_data)>0:
+			data.append(''.join(row_data))
+	workbook.Close(False)
+	excel.Quit()
+	return '\n'.join(data)
 
 
-	# Сохранение файла в другом формате с помощью win32com
-	excel_app = win32.Dispatch('Excel.Application')
-	wb_xl = excel_app.Workbooks.Open(file_source)
-	file_source += '.xlsx'
-	wb_xl.SaveAs(file_source, FileFormat=51)
-	wb_xl.Close()
-	excel_app.Quit()
 
-
-print('===============================================================')
 pattern_content = open(file='pattern.json', mode='r', encoding='utf-8').read()
-#print(pattern_content)
-print('===============================================================')
-
-workbook = load_workbook(file_source, data_only =True)
-
-print('=================================================================')
-
-sheet = workbook['Титульный']
-data = []
-for row in range(5,71+1):
-	st = ''
-	for col in range(11,11+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Титульный }}','\n'.join(data))
 
 
-print('=================================================================')
 
-sheet = workbook['Раздел_I__А']
-data = []
-for row in range(18,94+1):
-	st = ''
-	for col in range(105,198+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_I__А }}','\n'.join(data))
+echo(style('Титульный', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Титульный }}',read_excel_range(sys.argv[1],'Титульный', 5, 71, 11, 11))
+
+echo(style('Раздел_I__А', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_I__А }}',read_excel_range(sys.argv[1],'Раздел_I__А', 18, 94, 105, 198))
+
+echo(style('Раздел_I__Б', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_I__Б }}',read_excel_range(sys.argv[1],'Раздел_I__Б', 18, 94, 19, 19))
+
+echo(style('Раздел_I__В', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_I__В }}',read_excel_range(sys.argv[1],'Раздел_I__В', 18, 94, 28, 44))
+
+echo(style('Раздел_II__А_(ТИС)', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_II__А_(ТИС) }}',read_excel_range(sys.argv[1],'Раздел_II__А_(ТИС)', 18, 39, 67, 123))
+
+echo(style('Раздел_II__Б_(ТИС)', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_II__Б_(ТИС) }}',read_excel_range(sys.argv[1],'Раздел_II__Б_(ТИС)', 18, 39, 15, 15))
+
+echo(style('Раздел_III', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_III }}',read_excel_range(sys.argv[1],'Раздел_III', 18, 45, 12, 12))
+
+echo(style('Раздел_IV', bg='yellow', fg='bright_blue'))
+pattern_content = pattern_content.replace('{{ Раздел_IV }}',read_excel_range(sys.argv[1],'Раздел_IV', 18, 41, 12, 12))
 
 
-print('=================================================================')
+open(file=sys.argv[1]+'.json', mode='w', encoding='utf-8').write(pattern_content)
 
-sheet = workbook['Раздел_I__Б']
-data = []
-for row in range(18,94+1):
-	st = ''
-	for col in range(19, 19+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_I__Б }}','\n'.join(data))
 
-#print('=================================================================')
-
-sheet = workbook['Раздел_I__В']
-data = []
-for row in range(18,61+1):
-	st = ''
-	for col in range(28, 44+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_I__В }}','\n'.join(data))
-
-#print('=================================================================')
-
-sheet = workbook['Раздел_II__А_(ТИС)']
-data = []
-for row in range(18,39+1):
-	st = ''
-	for col in range(67, 123+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_II__А_(ТИС) }}','\n'.join(data))
-
-#print('=================================================================')
-
-sheet = workbook['Раздел_II__Б_(ТИС)']
-data = []
-for row in range(18,39+1):
-	st = ''
-	for col in range(15, 15+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_II__Б_(ТИС) }}','\n'.join(data))
-
-#print('=================================================================')
-
-sheet = workbook['Раздел_III']
-data = []
-for row in range(18,45+1):
-	st = ''
-	for col in range(12, 12+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_III }}','\n'.join(data))
-
-#print('=================================================================')
-
-sheet = workbook['Раздел_IV']
-data = []
-for row in range(18,41+1):
-	st = ''
-	for col in range(12, 12+1):
-		readed_value = sheet.cell(row=row, column=col).value
-		if readed_value != None:
-			st += readed_value
-	if len(st)>10:
-		data.append(st)
-#print('\n'.join(data))
-pattern_content = pattern_content.replace('{{ Раздел_IV }}','\n'.join(data))
-
-open(file=file_source+'.json', mode='a', encoding='utf-8').write(pattern_content)
-
-#input("Press Enter to continue...")
 
 
